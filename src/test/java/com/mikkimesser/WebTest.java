2fda7881a5d156d38feda63b58fc4be0f9991aa5
Mike Bulanov <mikki.messer@gmail.com>
2022-05-07 22:45:12 +0300

    added CSV Source and Method Source

--- src/test/java/com/mikkimesser/WebTest.java
+++ src/test/java/com/mikkimesser/WebTest.java
@@ -1,9 +1,15 @@
 package com.mikkimesser;
 
 import com.codeborne.selenide.*;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.google.common.base.Strings;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
@@ -19,7 +25,8 @@ public class WebTest {
         Configuration.browserSize = "1280x720";
     }
 
-    @DisplayName("Тест словаря Multitran")
+    @Disabled("Disabled Annotation Example")
+    @DisplayName("Простой тест словаря Multitran")
     @Test
     void testMultitranTranslation(){
         SelenideElement wordInput = $("#s");
@@ -35,5 +42,63 @@ public class WebTest {
         languageToDropdown.selectOption("Russian");
         searchButton.click();
         resultCells.find(Condition.text("локомотив")).shouldBe(visible);
+    }
+
+    @ValueSource(strings = {
+            "engine",
+            "car"
+    })
+    @ParameterizedTest(name = "Проверка перевода в мультитране с английского на русский слова {0}")
+    void testMultitranTranslationParametrizedSimple(String testData){
+        SelenideElement wordInput = $("#s");
+        SelenideElement languageFromDropdown = $("#l1");
+        SelenideElement languageToDropdown = $("#l2");
+        SelenideElement searchButton = $("input[type='submit']");
+        SelenideElement resultCell = $(".trans");
+
+        Selenide.open("https://multitran.ru");
+
+        wordInput.setValue(testData);
+        languageFromDropdown.selectOption("English");
+        languageToDropdown.selectOption("Russian");
+        searchButton.click();
+        resultCell.parent().parent().shouldHave(text(testData));
+    }
+
+    @CsvSource(value = {
+            "engine | локомотив",
+            "car | автомобиль (A four-wheeled motor vehicle used for land transport, usually propelled by a gasoline or diesel internal combustion engine"
+    },
+    delimiter = '|'
+    )
+    @ParameterizedTest(name = "Перевод в мультитране с английского на русский. Вход {0}, ожидаем {1}")
+    void testMultitranTranslationParametrizedComplex(String testData,
+                                                     String expectedResult){
+        SelenideElement wordInput = $("#s");
+        SelenideElement languageFromDropdown = $("#l1");
+        SelenideElement languageToDropdown = $("#l2");
+        SelenideElement searchButton = $("input[type='submit']");
+        ElementsCollection resultCells = $$(".trans");
+
+        Selenide.open("https://multitran.ru");
+
+        wordInput.setValue(testData);
+        languageFromDropdown.selectOption("English");
+        languageToDropdown.selectOption("Russian");
+        searchButton.click();
+        resultCells.find(Condition.text(expectedResult)).shouldBe(visible);
+    }
+    static Stream<Arguments> testMethodSourceExampleProvider() {
+        return Stream.of(
+                    Arguments.of(" ", true),
+                    Arguments.of("", true),
+                    Arguments.of("foo", false)
+                );
+    }
+
+    @MethodSource("testMethodSourceExampleProvider")
+    @ParameterizedTest(name = "Простой тест с аннотацией Method Source")
+    void testMethodSourceExample(String input, boolean expectedResult){
+        Assertions.assertEquals(expectedResult, Strings.isNullOrEmpty(input.trim()));
     }
 }
